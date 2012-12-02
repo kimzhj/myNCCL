e316b408de0e1458b28e1f79ce38a49d138fa8d2
kimzhj <zhj@ichinayun.com>
2012-12-02 22:01:58 +0800

    Update homeworks/2.c

--- homeworks/2.c
+++ homeworks/2.c
@@ -1,25 +1,19 @@
 #include<stdio.h>
 int main(void)
 {
-   int i,j;
+ int i,j;
 
    i=1;
-
    while(i < 10)
    {
      j=1;
-
      while(j <= i)
      {
-
        printf("%d*%d=%d\t",j,i,i*j);
        j++;
-
      } 
-
      printf("\n");
      i++;
-
    }
    return 0;
 }
