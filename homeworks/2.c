348187f76f495ebfd5ad899cbc5b8c1132bf7ca8
kimzhj <zhj@ichinayun.com>
2012-12-02 21:55:27 +0800

    2.c

---
+++ homeworks/2.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main(void)
+{
+   int i,j;
+
+   i=1;
+
+   while(i < 10)
+   {
+     j=1;
+
+     while(j <= i)
+     {
+
+       printf("%d*%d=%d\t",j,i,i*j);
+       j++;
+
+     } 
+
+     printf("\n");
+     i++;
+
+   }
+   return 0;
+}
